d4f1a4af8bd2a6295d3895807ad63c423b754dd8
Joy baidya <joybaidya1137@gmail.com>
2025-03-05 21:08:32 +0600

    Removed Main.java and Interaction_in_Selenium.java from assertion branch

--- src/main/java/org/example/Asseration.java
+++ src/main/java/org/example/Asseration.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class Asseration {
+
+    public static void main(String[] args) {
+        System.out.println("joy");
+    }
 }
--- src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(" Fast Branch contains All Web Elements Interaction in Selenium ");
-        System.out.println("2nd Branch contains Assertions and Reporting in Selenium using Java");
-    }
-}
